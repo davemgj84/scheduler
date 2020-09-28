319aae3bff308bd901bb230f92276f375acdf9a9
David <davemgj@gmail.com>
2020-09-28 16:06:01 -0700

    button and daylistitem stories completed and tested

---
+++ src/components/DayListItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+import classnames from 'classnames';
+
+import "components/DayListItem.scss";
+
+const DayListItem = (props) => {
+
+  const formatSpots = (spots) => {
+    return spots === 0 ? "no spots remaining" : spots === 1 ? "1 spot remaining" : `${spots} spots remaining`
+  };
+
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+};
+
+export default DayListItem;
