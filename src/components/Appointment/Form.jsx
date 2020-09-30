4ad9ef6f26b9da64f120c19164ef4a17b0531534
David <davemgj@gmail.com>
2020-09-29 17:46:31 -0700

    create edit and create form in appoitment component Form

---
+++ src/components/Appointment/Form.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Button from "components/Button";
+import InterviewerList from "components/InterviewerList";
+
+const Form = (props) => {
+
+  const [interviewer, setInterviewer] = useState(props.interviewer);
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            value={props.name}
+            placeholder="Enter Student Name"
+          />
+        </form>
+        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button onClick={props.onCancel} danger>Cancel</Button>
+          <Button onClick={props.onSave} confirm>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+};
+
+export default Form;
--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -166,5 +167,17 @@ storiesOf("Appointment", module)
     onClose={action("onClose")}
   />
   )
-
-
+  .add("Form Create", () => <Form
+    interviewers={interviewers}
+    onSave={action("onSave")}
+    onCancel={action("onCancel")}
+  />
+  )
+  .add("Form Edit", () => <Form
+    name={"David Jardine"}
+    interviewers={interviewers}
+    interviewer={interviewer.id}
+    onSave={action("onSave")}
+    onCancel={action("onCancel")}
+  />
+  );
